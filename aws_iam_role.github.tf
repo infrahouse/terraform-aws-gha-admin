735795c46ca9dbd7ae164dc154ed3e25c9d621d0
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-02-23 18:33:58 -0800

    Tag created resource with `created_by_module : "infrahouse/gha-admin/aws"`
    This will allow to track places where resources were created.

--- aws_iam_role.github.tf
+++ aws_iam_role.github.tf
@@ -3,6 +3,7 @@ resource "aws_iam_role" "github" {
   name               = "ih-tf-${var.repo_name}-github"
   description        = "Role for a GitHub Actions runner in repo ${var.repo_name}"
   assume_role_policy = data.aws_iam_policy_document.github-trust.json
+  tags               = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "github" {
