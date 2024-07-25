c3f3764c7e75051a6ffdd61964eed539ea14f27f
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-07-25 11:12:20 -0700

    Add max_session_duration variable with 12h default. (#14)
    A role has a one hour default session duration. It is not enough, as a `terraform apply` can run more than that.
    Adding the `max_session_duration` variable and set it to 12 hours as the default value.
    
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- aws_iam_role.github.tf
+++ aws_iam_role.github.tf
@@ -1,9 +1,10 @@
 resource "aws_iam_role" "github" {
-  provider           = aws.cicd
-  name               = "ih-tf-${var.repo_name}-github"
-  description        = "Role for a GitHub Actions runner in repo ${var.repo_name}"
-  assume_role_policy = data.aws_iam_policy_document.github-trust.json
-  tags               = local.tags
+  provider             = aws.cicd
+  name                 = "ih-tf-${var.repo_name}-github"
+  description          = "Role for a GitHub Actions runner in repo ${var.repo_name}"
+  assume_role_policy   = data.aws_iam_policy_document.github-trust.json
+  max_session_duration = var.max_session_duration
+  tags                 = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "github" {
